e4cf2197e0094110fdd4acbeeebc785b3362cbf4
nepslor <nepslor@gmail.com>
2022-07-20 14:03:00 +0200

    new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mbtr",
-    version="0.1.1",
+    version="0.1.2",
     author="Lorenzo Nespoli",
     author_email="lorenzo.nespoli@supsi.ch",
     description="Multivariate Boosted Trees Regressor package",
